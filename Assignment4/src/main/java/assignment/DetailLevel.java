c45fe88d31222e247a333638f01c36f619361134
kholifah-dina <kholifahdinaa@gmail.com>
2025-04-23 15:12:29 +0700

    refactoring

--- Assignment4/src/main/java/assignment/DetailLevel.java
+++
@@ -1,8 +0,0 @@
-package assignment;
-
-public enum DetailLevel {
-    SONG,      // Hanya info lagu
-    ARTIST,    // Info lagu + artis
-    ALBUM,     // Info lagu + album
-    FULL       // Semua informasi
-}
